8176ff6651478555caa5a0cf86e28c902b7f0ac2
ConnerGordon <2260684@flourbluffschools.org>
2026-03-20 13:20:45 -0500

    math and functions arent functioning

--- player/player.gd
+++ player/player.gd
@@ -1,14 +1,20 @@
 extends CharacterBody2D
 
 
-var SPEED = 300.0
+var SPEED = 400.0
 const JUMP_VELOCITY = -400.0
 
 enum state {idle,moving,attacking,dashing,dashattacking, jumping, airattacking,falling}
 
+enum airbornstate{grounded, falling}
+
 var current : state = state.idle
-var dashtimer := 20.0
+var aircurrent: airbornstate = airbornstate.falling
+var dashtimer := 40.0
 var dtb:= dashtimer
+var airdash := true
+
+
 
 var prevdir := 1.0
 
@@ -17,8 +23,11 @@ var prevdir := 1.0
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		current = state.falling
-		
+		velocity += get_gravity() * delta
+		aircurrent = airbornstate.falling
+	if is_on_floor():
+		airdash = true
+		aircurrent = airbornstate.grounded 
 	
 	
 	
@@ -26,84 +35,94 @@ func _physics_process(delta: float) -> void:
 	
 	match current:
 		state.moving:
+			print("move")
 			velocity.x = direction * SPEED
-			prevdir = direction
+			if prevdir != 0:
+				prevdir = direction
 		state.dashing:
+			#print("dash")
 			velocity.x = prevdir * SPEED * 4
 			dashtimer -= delta* 100
+			#print(dashtimer)
+			print(prevdir)
 			velocity.y = 0
 			if dashtimer > dtb/20:
 				if Input.is_action_just_pressed("swing"):
 					current = state.dashattacking
 			if dashtimer <= 0:
-				current= state.idle
+				if aircurrent == airbornstate.falling:
+					current = state.falling
+				else:
+					current= state.idle
 				dashtimer = dtb
-				
-				
-				
 		state.jumping:
-			
+			#print("jump")
+			aircurrent = airbornstate.falling
 			if is_on_floor():
-				
 				velocity.y -= 500
-				
-			
-			
+			if velocity.y < 0:
+				current = state.falling
 		state.dashattacking:
 			current = state.idle
-		
 		state.falling:
-			
-			velocity += get_gravity() * delta
-			var accel = direction * SPEED * delta*5
-			if momentcancel(accel):
-				velocity.x = clampf(velocity.x,-SPEED/1.5,SPEED/1.5)
-			else:
+			#print("falling")
+			var accel = direction * SPEED * delta*10
+			if momentconserv(accel):
 				velocity.x += accel
-				
 			
-				
+			if direction != 0:
+				prevdir = direction
 			if is_on_floor():
 				current = state.idle
-			
-			
-			
-			
-			
-			
 		_:
-			dashtimer = 20.0
+			#print("idle/default")
+			dashtimer = dtb
 			SPEED= 300.0
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 		
+	match aircurrent:
+
+		airbornstate.grounded:
+			if current != state.dashing:
+				if direction != 0:
+					current = state.moving
+			if Input.is_action_just_pressed("dash"):
+				current = state.dashing
+			
+			
+			if Input.is_action_just_pressed("jump"):
+				current = state.jumping
+				aircurrent = airbornstate.falling
+				
+			if Input.is_action_pressed("movementkeys") == false:
+				
+				current = state.idle
+				
+			
+		airbornstate.falling:
+			if Input.is_action_just_pressed("dash"):
+				if airdash:
+					current = state.dashing
+					airdash= !airdash
+			if dashtimer != dtb && Input.is_action_pressed("movementkeys") == false:
+				current = state.falling
 	
 	
-	if Input.is_action_just_pressed("dash"):
-		current = state.dashing
-	
-	
-	if Input.is_action_just_pressed("jump"):
-		current = state.jumping
-	
-	
-	if current == state.idle:
-		if direction != 0:
-			current = state.moving
 	
 	
 	
 	
-	
-		if Input.is_action_pressed("movementkeys") == false:
-			current = state.idle
-	
-	#print(current)
+	##print("playerint state: 	"+ str(current))
+	##print("airborne:	 " +str(aircurrent))
 	move_and_slide()
 
 
 
-func momentcancel(acc: float)-> bool:
-	if abs(velocity.x) + abs(acc) > SPEED/1.5 && abs(velocity.x) < SPEED/1.5:
+func momentconserv(acc: float)-> bool:
+	##print("vel:" + str(velocity.x))
+	##print("acc:" + str(acc))
+	##print(SPEED/1.5)
+	if abs(velocity.x + acc) < SPEED/1.5 || abs(velocity.x + acc) < abs(velocity.x):
 		return true
 	return false
