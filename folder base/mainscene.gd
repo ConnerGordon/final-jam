115d113afa23023e60d44124052eb090f77fabd0
ConnerGordon <2260684@flourbluffschools.org>
2026-03-25 13:06:43 -0500

    full terrain adding and endless hall added

--- folder base/mainscene.gd
+++ folder base/mainscene.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
+@onready var nextmappoint: Marker2D = $nextmappoint
+
 
 var basehall1 : PackedScene = preload("res://folder base/basehall1.tscn")
 var fighthall1 : PackedScene = preload("res://folder base/fightinghall1.tscn")
 var uppath: PackedScene = preload("res://folder base/upwardpath.tscn")
 var downpath: PackedScene = preload("res://folder base/downpath.tscn")
 
-var roomarray : Array[PackedScene] = []
+var roomarray : Array[PackedScene] = [fighthall1,uppath,downpath]
 
 
 
@@ -17,13 +19,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("interact"):
+		spawnroom()
 
 
 
 
 func spawnroom()->void:
-	pass
+	var preloadscene = roomarray[randf_range(0,3)]
+	var loaded = preloadscene.instantiate()
+	add_child(loaded)
+	loaded.global_position = nextmappoint.global_position
+	nextmappoint.global_position = loaded.get_child(0).global_position
+	
 	
 	
 	
--- player/player.gd
+++ player/player.gd
@@ -15,7 +15,8 @@ extends CharacterBody2D
 
 
 var SPEED = 400.0
-const JUMP_VELOCITY = -400.0
+var jumpdefault=1000.0
+var JUMP_VELOCITY = jumpdefault
 
 
 enum state {idle,moving,attacking,dashing,dashattacking, jumping, airattacking,falling}
@@ -71,7 +72,6 @@ func _physics_process(delta: float) -> void:
 				if Input.is_action_just_pressed("swing") && aircurrent == airbornstate.grounded:
 					current = state.dashattacking
 			if dashtimer <= 0:
-				print(aircurrent)
 				if aircurrent == airbornstate.falling:
 					velocity.x = clampf(velocity.x,-SPEED/1.2,SPEED/1.2)
 					current = state.falling
@@ -83,7 +83,7 @@ func _physics_process(delta: float) -> void:
 			#print("jump")
 			aircurrent = airbornstate.falling
 			if is_on_floor():
-				velocity.y -= 500
+				velocity.y -= JUMP_VELOCITY
 			if velocity.y < 0:
 				current = state.falling
 		state.dashattacking:
@@ -124,9 +124,17 @@ func _physics_process(delta: float) -> void:
 			
 			if Input.is_action_just_pressed("jump"):
 				
+				
+				
+				if current == state.dashing:
+					JUMP_VELOCITY = JUMP_VELOCITY/3
+				else: 
+					JUMP_VELOCITY = jumpdefault
 				current = state.jumping
 				aircurrent = airbornstate.falling
 				dashtimer = dtb
+				
+				
 				
 			if Input.is_action_pressed("movementkeys") == false:
 				
