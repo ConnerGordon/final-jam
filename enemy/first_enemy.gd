169b1c4bedb864ae5a5295598eb8160700758780
ConnerGordon <2260684@flourbluffschools.org>
2026-04-15 12:09:20 -0500

    enemy progress
    progress is slow due to calculations for the jump being complex

--- enemy/first_enemy.gd
+++ enemy/first_enemy.gd
@@ -30,6 +30,8 @@ var uptime := false
 var start:= Vector2.ZERO
 var end:= Vector2.ZERO
 
+var gravsig = 9.8*2.5
+
 func _ready() -> void:
 	var_health = 5
 	navigation_agent_2d.link_reached.connect(func(dict:Dictionary):
@@ -50,7 +52,7 @@ func _physics_process(delta: float) -> void:
 	
 	
 	if not is_on_floor():
-		velocity.y += 9.8 * 2.5
+		velocity.y += gravsig
 		
 	
 	
@@ -175,9 +177,10 @@ func get_new_target():
 	
 
 func jump(star:Vector2, en:Vector2):
-	print(abs((star.y-en.y)*9.8*2.5))
-	print((star.y-en.y))
-	velocity.y = -abs((star.y-en.y)*9.8*2.5)
+	var dif = star-en
+	velocity.y = -abs(
+		gravsig
+		)
 func set_idle():
 	idle_timer = randf_range(2,20)
 	cur = state.idle
